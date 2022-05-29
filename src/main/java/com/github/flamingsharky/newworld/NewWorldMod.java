f99ace8e57e572f356ce64bbdf2fff893ea9f826
FlamingSharky <53378377+FlamingSharky@users.noreply.github.com>
2022-05-29 14:02:20 -0400

    I think the general thing should be done, I just need to get the actual run configs ready and then it's onto the real thing.

---
+++ src/main/java/com/github/flamingsharky/newworld/NewWorldMod.java
@@ -0,0 +1,46 @@
+package com.github.flamingsharky.newworld;
+
+
+import com.github.flamingsharky.newworld.setup.ClientSetup;
+import com.github.flamingsharky.newworld.setup.ModSetup;
+import com.github.flamingsharky.newworld.setup.Registration;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.loading.FMLEnvironment;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(NewWorldMod.MOD_ID)
+public class NewWorldMod
+{
+    private static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "newworld";
+
+    public NewWorldMod() {
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        eventBus.addListener(ModSetup::init);
+        if (FMLEnvironment.dist == Dist.CLIENT) {
+            eventBus.addListener(ClientSetup::init);
+        }
+
+        Registration.init();
+    }
+
+    public static final CreativeModeTab NewWorldModTab = new CreativeModeTab("newworldcreativetab") {
+        @Override
+        public ItemStack makeIcon()
+        {
+            return new ItemStack(Registration.FIR_SAPLING.get());
+        }
+    };
+}
